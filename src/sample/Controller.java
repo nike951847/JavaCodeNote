8cb64eef1504da797f184b550c3f907b576afc3c
yushliu <kenjohn2002@gmail.com>
2022-04-22 02:55:18 +0800

    push

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,8 +3,13 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Line;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 
 public class Controller {
 
@@ -66,45 +71,88 @@ public class Controller {
     private GridPane desktop;
 
     public void initialize() {
+        {
+            openMPStation.setImage(Main.imageVector.get(0));
+            inheritanceStation.setImage(Main.imageVector.get(1));
+            polymorphismStation.setImage(Main.imageVector.get(2));
+            encapsulationStation.setImage(Main.imageVector.get(3));
+            mPIStation.setImage(Main.imageVector.get(4));
+            classStation.setImage(Main.imageVector.get(5));
+            interfaceStation.setImage(Main.imageVector.get(6));
+            exceptionStation.setImage(Main.imageVector.get(7));
+            threadStation.setImage(Main.imageVector.get(8));
+            centralStation.setImage(Main.imageVector.get(9));
+            dFS_BFSStation.setImage(Main.imageVector.get(10));
+            distributedStation.setImage(Main.imageVector.get(11));
+            gPUStation.setImage(Main.imageVector.get(12));
+            dataTypeStation.setImage(Main.imageVector.get(13));
+            hashStation.setImage(Main.imageVector.get(14));
+            treeStation.setImage(Main.imageVector.get(15));
+            algorithmStation.setImage(Main.imageVector.get(16));
+            graphStation.setImage(Main.imageVector.get(17));
+        }
 
-        openMPStation.setImage(Main.imageVector.get(0));
-        inheritanceStation.setImage(Main.imageVector.get(1));
-        polymorphismStation.setImage(Main.imageVector.get(2));
-        encapsulationStation.setImage(Main.imageVector.get(3));
-        mPIStation.setImage(Main.imageVector.get(4));
-        classStation.setImage(Main.imageVector.get(5));
-        interfaceStation.setImage(Main.imageVector.get(6));
-        exceptionStation.setImage(Main.imageVector.get(7));
-        threadStation.setImage(Main.imageVector.get(8));
-        centralStation.setImage(Main.imageVector.get(9));
-        dFS_BFSStation.setImage(Main.imageVector.get(10));
-        distributedStation.setImage(Main.imageVector.get(11));
-        gPUStation.setImage(Main.imageVector.get(12));
-        dataTypeStation.setImage(Main.imageVector.get(13));
-        hashStation.setImage(Main.imageVector.get(14));
-        treeStation.setImage(Main.imageVector.get(15));
-        algorithmStation.setImage(Main.imageVector.get(16));
-        graphStation.setImage(Main.imageVector.get(17));
+        {
+            openMPStation.setFitHeight(40);
+            inheritanceStation.setFitHeight(40);
+            polymorphismStation.setFitHeight(40);
+            encapsulationStation.setFitHeight(40);
+            mPIStation.setFitHeight(40);
+            classStation.setFitHeight(40);
+            interfaceStation.setFitHeight(40);
+            exceptionStation.setFitHeight(40);
+            threadStation.setFitHeight(40);
+            centralStation.setFitHeight(40);
+            dFS_BFSStation.setFitHeight(40);
+            distributedStation.setFitHeight(40);
+            gPUStation.setFitHeight(40);
+            dataTypeStation.setFitHeight(40);
+            hashStation.setFitHeight(40);
+            treeStation.setFitHeight(40);
+            algorithmStation.setFitHeight(40);
+            graphStation.setFitHeight(40);
+        }
 
-        openMPStation.setFitHeight(40);
-        inheritanceStation.setFitHeight(40);
-        polymorphismStation.setFitHeight(40);
-        encapsulationStation.setFitHeight(40);
-        mPIStation.setFitHeight(40);
-        classStation.setFitHeight(40);
-        interfaceStation.setFitHeight(40);
-        exceptionStation.setFitHeight(40);
-        threadStation.setFitHeight(40);
-        centralStation.setFitHeight(40);
-        dFS_BFSStation.setFitHeight(40);
-        distributedStation.setFitHeight(40);
-        gPUStation.setFitHeight(40);
-        dataTypeStation.setFitHeight(40);
-        hashStation.setFitHeight(40);
-        treeStation.setFitHeight(40);
-        algorithmStation.setFitHeight(40);
-        graphStation.setFitHeight(40);
+        {
+            Canvas canvas = new Canvas(300, 250);
+            GraphicsContext gc = canvas.getGraphicsContext2D();
+            drawShapes(gc, 1);
+            desktop.add(canvas, 7, 4);
 
-        //desktop.
+            canvas = new Canvas(300, 250);
+            gc = canvas.getGraphicsContext2D();
+            drawShapes(gc, 1);
+            desktop.add(canvas, 7, 10);
+
+            canvas = new Canvas(300, 250);
+            gc = canvas.getGraphicsContext2D();
+            drawShapes(gc, 1);
+            desktop.add(canvas, 7, 16);
+        }
+
+    }
+
+    private void drawShapes(GraphicsContext gc, int type) {
+
+        switch(type) {
+            case 1:
+                gc.setFill(Color.YELLOW);
+                gc.setStroke(Color.YELLOW);
+                gc.setLineWidth(10);
+                gc.strokeLine(20, 15, 20, 235);
+                break;
+            case 8:
+                System.out.println("得B");
+                break;
+            case 7:
+                System.out.println("得C");
+                break;
+            case 6:
+                System.out.println("得D");
+                break;
+            default:
+                System.out.println("");
+        }
+
     }
 }
