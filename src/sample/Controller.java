2b55d00fecd6b8d54b1218dcf49257b783aee129
yushliu <kenjohn2002@gmail.com>
2022-04-22 02:31:28 +0800

    push

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,110 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.scene.layout.GridPane;
+
 public class Controller {
+
+    @FXML
+    private ImageView algorithmStation;
+
+    @FXML
+    private ImageView centralStation;
+
+    @FXML
+    private ImageView classStation;
+
+    @FXML
+    private ImageView dFS_BFSStation;
+
+    @FXML
+    private ImageView dataTypeStation;
+
+    @FXML
+    private ImageView distributedStation;
+
+    @FXML
+    private ImageView encapsulationStation;
+
+    @FXML
+    private ImageView exceptionStation;
+
+    @FXML
+    private ImageView gPUStation;
+
+    @FXML
+    private ImageView graphStation;
+
+    @FXML
+    private ImageView hashStation;
+
+    @FXML
+    private ImageView inheritanceStation;
+
+    @FXML
+    private ImageView interfaceStation;
+
+    @FXML
+    private ImageView mPIStation;
+
+    @FXML
+    private ImageView openMPStation;
+
+    @FXML
+    private ImageView polymorphismStation;
+
+    @FXML
+    private ImageView threadStation;
+
+    @FXML
+    private ImageView treeStation;
+
+    @FXML
+    private GridPane desktop;
+
+    public void initialize() {
+
+        openMPStation.setImage(Main.imageVector.get(0));
+        inheritanceStation.setImage(Main.imageVector.get(1));
+        polymorphismStation.setImage(Main.imageVector.get(2));
+        encapsulationStation.setImage(Main.imageVector.get(3));
+        mPIStation.setImage(Main.imageVector.get(4));
+        classStation.setImage(Main.imageVector.get(5));
+        interfaceStation.setImage(Main.imageVector.get(6));
+        exceptionStation.setImage(Main.imageVector.get(7));
+        threadStation.setImage(Main.imageVector.get(8));
+        centralStation.setImage(Main.imageVector.get(9));
+        dFS_BFSStation.setImage(Main.imageVector.get(10));
+        distributedStation.setImage(Main.imageVector.get(11));
+        gPUStation.setImage(Main.imageVector.get(12));
+        dataTypeStation.setImage(Main.imageVector.get(13));
+        hashStation.setImage(Main.imageVector.get(14));
+        treeStation.setImage(Main.imageVector.get(15));
+        algorithmStation.setImage(Main.imageVector.get(16));
+        graphStation.setImage(Main.imageVector.get(17));
+
+        openMPStation.setFitHeight(40);
+        inheritanceStation.setFitHeight(40);
+        polymorphismStation.setFitHeight(40);
+        encapsulationStation.setFitHeight(40);
+        mPIStation.setFitHeight(40);
+        classStation.setFitHeight(40);
+        interfaceStation.setFitHeight(40);
+        exceptionStation.setFitHeight(40);
+        threadStation.setFitHeight(40);
+        centralStation.setFitHeight(40);
+        dFS_BFSStation.setFitHeight(40);
+        distributedStation.setFitHeight(40);
+        gPUStation.setFitHeight(40);
+        dataTypeStation.setFitHeight(40);
+        hashStation.setFitHeight(40);
+        treeStation.setFitHeight(40);
+        algorithmStation.setFitHeight(40);
+        graphStation.setFitHeight(40);
+
+        //desktop.
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,16 +4,76 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import java.util.*;
 
 public class Main extends Application {
 
+    static public Vector<Image> imageVector = new Vector<Image>();
+    static public Vector<String> stationName = new Vector<String>();
+    static final int stationNum = 18;//0~17
+
+    /*
+    * 0: OpenMPStation
+    * 1: InheritanceStation
+    * 2: PolymorphismStation
+    * 3: EncapsulationStation
+    * 4: MPIStation
+    * 5: ClassStation
+    * 6: InterfaceStation
+    * 7: ExceptionStation
+    * 8: ThreadStation
+    * 9: CentralStation
+    * 10: DFS_BFSStation
+    * 11: DistributedStation
+    * 12: GPUStation
+    * 13: DataTypeStation
+    * 14: HashStation
+    * 15: TreeStation
+    * 16: AlgorithmStation
+    * 17: GraphStation
+    * */
+
+    //initialize stationName to avoid typo
+    static  {
+        stationName.add("OpenMPStation");//0
+        stationName.add("InheritanceStation");//1
+        stationName.add("PolymorphismStation");//2
+        stationName.add("EncapsulationStation");//3
+        stationName.add("MPIStation");//4
+        stationName.add("ClassStation");//5
+        stationName.add("InterfaceStation");//6
+        stationName.add("ExceptionStation");//7
+        stationName.add("ThreadStation");//8
+        stationName.add("CentralStation");//9
+        stationName.add("DFS_BFSStation");//10
+        stationName.add("DistributedStation");//11
+        stationName.add("GPUStation");//12
+        stationName.add("DataTypeStation");//13
+        stationName.add("HashStation");//14
+        stationName.add("TreeStation");//15
+        stationName.add("AlgorithmStation");//16
+        stationName.add("GraphStation");//17
+    }
+
+    //initialize imageVector
+    static {
+        for(int i=0; i<stationNum; i++) {
+            imageVector.add(new Image("file:src/sample/photo/"+stationName.get(i)+".jpg"));//0
+        }
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("EXAMPLE");
+        primaryStage.setScene(new Scene(root, 1250, 600));
         primaryStage.show();
+
     }
 
 
